2f6f70f21114b47654e07669891a986078aeda2e
ZYWER-Oskar <z019897m@student.staffs.ac.uk>
2025-05-24 19:14:46 +0100

    changed game name

--- src/data.ts
+++ src/data.ts
@@ -16,7 +16,7 @@ export const PersonalInfo: AboutMeData = {
 
 export const games: Game[] = [
   {
-    name: "GAME_NAME",
+    name: "BRUDDAAAAAAAA",
     description: "GAME_DESCRIPTION",
     genres: ["GAME_GENRE", "GAME_GENRE..."],
     platforms: [Platform.Windows],
